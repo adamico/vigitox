e6e99b1e4eb8081a4bba039b6acb92df0848d451
Andrea <andrea.damico@gmail.com>
2010-02-03 15:57:09 +0100

    added rspec

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+Factory.define :revue do |f|
+  f.sequence(:numero) {|n| n}
+  f.date_sortie Time.now.to_date
+end
+
+Factory.define(:article) do |a|
+  a.sequence(:titre) {|n| "article#{n}"}
+  a.association :revue
+end
--- test/factories.rb
+++
@@ -1,5 +0,0 @@
-Factory.define(:revue) do |f|
-  f.sequence(:numero) {|n| n}
-  f.date_sortie Time.now.to_date
-end
-
