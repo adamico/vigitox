bb35b6811dad6b5eeaab3a2f439bac7c1c727606
Andrea <andrea.damico@gmail.com>
2010-07-13 13:17:46 +0200

    fixed drag and drop not working

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,14 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com'
+  config.gem 'formtastic'
   config.gem 'paperclip', :source => 'http://gemcutter.org'
   config.gem 'searchlogic'
   config.gem "prawn"
