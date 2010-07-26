4bf2fd74a0134b5ae88b8f728070d242c2e47623
Andrea <andrea.damico@gmail.com>
2010-07-26 19:50:24 +0200

    invoked rails new in app root dir

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '8c2bd76036ac3ff7e24778e20c0385adbfe728b680cc5007950856657637b7dc470c08d25e5ce3d384a4e8b94e67c496e1e7450551446314e6b04de1cdf69fc4'
