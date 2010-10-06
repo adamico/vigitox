c7f7ee6ba52f7c14266b3e77a17a23222f357eeb
Andrea <andrea.damico@gmail.com>
2010-10-06 18:12:43 +0200

    added bundler setup

---
+++ Gemfile
@@ -0,0 +1,24 @@
+# Edit this Gemfile to bundle your application's dependencies.
+# This preamble is the current preamble for Rails 3 apps; edit as needed.
+source 'http://rubygems.org'
+
+gem 'rails', '2.3.9'
+gem 'haml', '3.0.18'
+gem "RedCloth"
+gem 'searchlogic'
+gem 'formtastic'
+gem 'will_paginate'
+gem 'paperclip'
+gem 'prawn'
+gem 'acts_as_list'
+gem 'devise', '1.0.7'
+
+gem 'pg', :group => :production
+
+group :development do
+  gem 'sqlite3-ruby', :require => 'sqlite3'
+  gem "nifty-generators"
+  gem "wirble"
+  gem "hirb"
+  gem 'annotate-models', '1.0.4'
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,12 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem 'formtastic'
-  config.gem 'paperclip', :source => 'http://gemcutter.org'
-  config.gem 'searchlogic'
-  config.gem "prawn"
-  config.gem 'ryanb-acts-as-list', :lib => 'acts_as_list', :source => 'http://gems.github.com'
-  config.gem 'devise', :version => '1.0.6'
-  config.gem "repeated_auto_complete"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
