f01a7681364813800ddeb9ea7a0cb691a4c663a4
Andrea D'Amico <andrea.damico@gmail.com>
2012-05-31 12:13:24 +0200

    adding custom save actions for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'stringex'
 class ArticlesController < InheritedResources::Base
   respond_to :html, :json
@@ -17,5 +19,29 @@ class ArticlesController < InheritedResources::Base
     @prev = resource_class.prev(@article_by_position).first
     @next = resource_class.next(@article_by_position).first
     show!
+  end
+
+  def create
+    create! do |success, failure|
+      success.html { redirect_on_success }
+    end
+  end
+
+  def update
+    update! do |success, failure|
+      success.html { redirect_on_success }
+    end
+  end
+
+  private
+
+  def redirect_on_success
+    if params[:_save_and_continue]
+      redirect_to edit_revue_article_path(parent, resource), notice: "L'article a été sauvegardé avec succès."
+    elsif params[:_save_and_add_another]
+      redirect_to new_revue_article_path(parent), notice: "L'article #{resource.full_title} a été sauvegardé."
+    else
+      redirect_to resource, notice: "L'article a été sauvegardé."
+    end
   end
 end
