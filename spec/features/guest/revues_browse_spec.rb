bae967bcffab7fb40f0f8671ff0948f2d243f8bb
Andrea D'Amico <andrea.damico@gmail.com>
2013-07-17 10:25:04 +0200

    refactoring articles#search, specs for search, argument search field

--- spec/features/guest/revues_browse_spec.rb
+++ spec/features/guest/revues_browse_spec.rb
@@ -24,4 +24,26 @@ feature "Browsing revues" do
     page.should have_content /vigitox numéro 4/
     page.should_not have_link "Numéro suivant"
   end
+
+  describe "search" do
+    given!(:revue)           {create(:revue)}
+    given!(:argumentaire)    {create(:argumentaire, main_argument: create(:argument, name: "argument"))}
+
+    (1..3).each do |i|
+      given!(:"article#{i}") {create(:article, titre: "titre de l'article#{i}",
+                                     contenu: "contenu de l'article#{i}",
+                                     revue: revue, argumentaire: argumentaire)}
+    end
+
+    %w(titre contenu argument).each do |field|
+      scenario "by keyword in #{field} field" do
+        visit root_path
+        fill_in "search", with: "#{field}"
+        click_on "search_button"
+        (1..3).each do |i|
+          page.should have_content("titre de l'article#{i}")
+        end
+      end
+    end
+  end
 end
