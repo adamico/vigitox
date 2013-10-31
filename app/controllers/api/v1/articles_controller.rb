0a5ffa1d6c38f214efd4d848b81fa4330c158042
Andrea D'Amico <andrea.damico@gmail.com>
2013-10-31 14:06:28 +0100

    adding api authentication with a token

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,10 +1,19 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
+      before_filter :restrict_access
       respond_to :json
 
       def index
         respond_with Article.all
+      end
+
+      private
+
+      def restrict_access
+        authenticate_or_request_with_http_token do |token, options|
+          ApiKey.exists?(access_token: token)
+        end
       end
     end
   end
--- spec/controllers/api/v1/articles_controller_spec.rb
+++ spec/controllers/api/v1/articles_controller_spec.rb
@@ -2,8 +2,20 @@ require 'spec_helper'
 
 describe Api::V1::ArticlesController do
   describe "GET :index" do
-    it "should be successful" do
+    let!(:token) { ApiKey.create!.access_token }
+    it "should respond with unauthorized status without a token" do
       get :index, format: :json
+      expect(response.status).to eq(401)
+    end
+    it "should respond with unauthorized status with an invalid token" do
+      request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials("blablabla123")
+      get :index, format: :json
+      expect(response.status).to eq(401)
+    end
+    it "should be successful with an existing api token" do
+      request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(token)
+      get :index, format: :json
+      expect(response.status).to eq(200)
     end
   end
 end
