36c2ba831c55325bdc72f7c96bbc9f656f534588
Andrea D'Amico <andrea.damico@gmail.com>
2013-11-05 17:23:09 +0100

    removing token

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,14 +1,13 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
-      before_action :restrict_access
       protect_from_forgery except: :index
 
       respond_to :json
 
       def index
         @articles = Article.search(params[:q]).includes(:revue).order("revue_id DESC")
-        respond_with @articles.map(&:titre_and_revue).to_json
+        respond_with @articles.map(&:titre_and_revue)
       end
 
       private
