237f802647bd8594c9e56971b822671268bc3dbf
Andrea D'Amico <andrea.damico@gmail.com>
2013-12-20 17:30:46 +0100

    removing precompile images

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Vigitox
     config.assets.enabled = true
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w(ckeditor/*)
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
