8a3d5e5f4b71518846c5c4c0927daf873b5cb3cf
Andrea D'Amico <andrea.damico@gmail.com>
2014-07-18 17:08:25 +0200

    regenerate ckeditor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718121126) do
+ActiveRecord::Schema.define(version: 20140718170754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
